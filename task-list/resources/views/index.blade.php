d5e9cf842dbb2890740ff8bf835459dbf210ef36
Alex <yuriev200@gmail.com>
2024-11-12 09:44:06 +0200

    route & blade file for task list, placeholder for selected task

---
+++ task-list/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<h1>The list of tasks</h1>
+
+<div>
+    @forelse ($tasks as $task)
+        <div>
+            <a href={{route('tasks.show', ['id' => $task->id])}}>
+                {{ $task->title }}
+            </a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+</div>
