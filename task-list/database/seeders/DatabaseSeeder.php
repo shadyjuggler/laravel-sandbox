e8c55a05092791354fb33566356c0d41895e4d11
Alex <yuriev200@gmail.com>
2024-11-12 11:53:21 +0200

    TaskFactory & seeder

--- task-list/database/seeders/DatabaseSeeder.php
+++ task-list/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Task;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,10 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Task::factory(20)->create();
     }
 }
