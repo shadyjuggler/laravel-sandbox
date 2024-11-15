ca0b3e971a666a2b0cbeac7e1397b6ed51d1f4c5
Alex <yuriev200@gmail.com>
2024-11-15 11:49:33 +0200

    add pagination for tasks.index

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -13,7 +13,7 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => \App\Models\Task::latest()->paginate()
+        'tasks' => \App\Models\Task::latest()->paginate(10)
     ]);
 })->name('tasks.index');
 
