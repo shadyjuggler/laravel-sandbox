fd00ddb50251df2ba466016ee8d431dbca4cfb45
Alex <yuriev200@gmail.com>
2024-11-17 15:13:22 +0200

    logic for filtering

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -13,12 +13,22 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
+        $filter = $request->input('filter', '');
 
         $books = Book::when(
             $title,
             fn($query, $title) => $query->title($title)
-        )
-            ->get();
+        );
+
+        $books = match ($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6_months' => $books->popularLastSixMonth(),
+            'highest_rated_last_month' => $books->HighestRatedLastMonth(),
+            'highest_rated_last_6_months' => $books->HighestRatedLastSixMonth(),
+            default => $books->latest()
+        };
+
+        $books = $books->get();
 
         return view('books.index', ['books' => $books]);
     }
--- book-review/app/Models/Book.php
+++ book-review/app/Models/Book.php
@@ -32,7 +32,8 @@ class Book extends Model
             ->orderBy('reviews_avg_rating', 'desc');
     }
 
-    public function scopeMinReviews(Builder $query, int $minReviews): Builder {
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder
+    {
         return $query->having('reviews_count', '>=', $minReviews);
     }
 
@@ -45,5 +46,33 @@ class Book extends Model
         } elseif ($from && $to) {
             $query->whereBetween('created_at', [$from, $to]);
         }
+    }
+
+    public function scopePopularLastMonth(Builder $query): Builder
+    {
+        return $query->popular(now()->subMonth(), now())
+            ->highestRated(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopePopularLastSixMonth(Builder $query): Builder
+    {
+        return $query->popular(now()->subMonths(6), now())
+            ->highestRated(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
+
+    public function scopeHighestRatedLastMonth(Builder $query): Builder
+    {
+        return $query->highestRated(now()->subMonth(), now())
+            ->popular(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopeHighestRatedLastSixMonth(Builder $query): Builder
+    {
+        return $query->highestRated(now()->subMonths(6), now())
+            ->popular(now()->subMonths(6), now())
+            ->minReviews(5);
     }
 }
