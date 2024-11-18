2d73090c7615fe5097eb5ef5643b17813132aedd
Alex <yuriev200@gmail.com>
2024-11-18 18:25:44 +0200

    Model events register for updating & deleting review

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -29,9 +29,7 @@ class BookController extends Controller
         };
 
         $cacheKey = 'books_' . $filter . ':' . $title;
-        $books = cache()->remember($cacheKey, 3600, function () use($books) {
-            return $books->get();
-        });
+        $books = cache()->remember($cacheKey, 3600, fn () => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
@@ -57,14 +55,13 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        return view(
-            'books.show',
-            [
-                'book' => $book->load([
-                    'reviews' => fn ($query) => $query->latest()
-                ])
-            ]
-        );
+        $cacheKey = 'book:' . $book->id;
+
+        $book = cache()->remember($cacheKey, 3600, fn () => $book->load([
+            'reviews' => fn ($query) => $query->latest()
+        ]));
+
+        return view('books.show', [ 'book' => $book]);
     }
 
     /**
