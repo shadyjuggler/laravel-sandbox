c2321c8945e7f24b2d895ac74d557ed2e3502486
Alex <yuriev200@gmail.com>
2024-11-30 16:05:17 +0200

    redicrect from / to /books

--- book-review/routes/web.php
+++ book-review/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return redirect()->route('books.index');
 });
 
 Route::resource('books', BookController::class);
