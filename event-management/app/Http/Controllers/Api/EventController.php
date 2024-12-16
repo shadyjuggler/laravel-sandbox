b4b0bda8c949235d7d73a6e61290c2aa8e7b5707
Alex <yuriev200@gmail.com>
2024-12-16 09:31:44 +0200

    request parameter to load the model relations

--- event-management/app/Http/Controllers/Api/EventController.php
+++ event-management/app/Http/Controllers/Api/EventController.php
@@ -14,9 +14,36 @@ class EventController extends Controller
      */
     public function index()
     {
+        $query = Event::query();
+        $relations = [
+            'user',
+            'attendees',
+            'attendees.user'
+        ];
+
+        foreach ($relations as $relation) {
+            $query->when(
+                $this->shouldIncludeRealation($relation),
+                fn($q) => $q->with($relation)
+            );
+        }
+
         return EventResource::collection(
-            Event::with('user')->paginate()
+            $query->latest()->paginate()
         );
+    }
+
+    protected function shouldIncludeRealation(string $relation): bool
+    {
+        $include = request()->query('include');
+
+        if (!$include) {
+            return false;
+        }
+
+        $relations = array_map('trim', explode(',', $include));
+
+        return in_array($relation, $relations);
     }
 
     /**
