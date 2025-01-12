49012baa18fcb1f57c96dd111d380e69302362df
Alex <yuriev200@gmail.com>
2025-01-12 13:30:44 +0200

    job index view & card, tag, layout components

--- job-board/app/Http/Controllers/JobController.php
+++ job-board/app/Http/Controllers/JobController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Job;
 
 class JobController extends Controller
 {
@@ -11,7 +12,7 @@ class JobController extends Controller
      */
     public function index()
     {
-        //
+        return view('job.index', ['jobs' => Job::all()]);
     }
 
     /**
--- job-board/resources/views/components/layout.blade.php
+++ job-board/resources/views/components/layout.blade.php
@@ -12,8 +12,8 @@
     @vite('resources/css/app.css')
 </head>
 
-<body>
-    <div class="text-2xl">wasup</div>
+<body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
+    {{$slot}}
 </body>
 
 </html>
