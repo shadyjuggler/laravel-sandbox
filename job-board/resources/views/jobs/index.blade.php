2534ed5031a3b5f3d879155af8712121fec49869
Alex <yuriev200@gmail.com>
2025-01-12 16:07:08 +0200

    jobs filters by experience & category

--- job-board/resources/views/jobs/index.blade.php
+++ job-board/resources/views/jobs/index.blade.php
@@ -5,13 +5,16 @@
     />
 
     <x-card class="mb-4 text-sm">
-        <form action="{{route('jobs.index')}}" method="GET">
+        <form
+            action="{{ route('jobs.index') }}"
+            method="GET"
+        >
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Search</div>
                     <x-text-input
                         name='search'
-                        value="{{request('search')}}"
+                        value="{{ request('search') }}"
                         placeholder='Search for any text'
                     />
                 </div>
@@ -20,19 +23,36 @@
                     <div class="flex gap-2">
                         <x-text-input
                             name='min_salary'
-                            value="{{request('min_salary')}}"
+                            value="{{ request('min_salary') }}"
                             placeholder='From'
                         />
                         <x-text-input
                             name='max_salary'
-                            value="{{request('max_salary')}}"
+                            value="{{ request('max_salary') }}"
                             placeholder='To'
                         />
                     </div>
                 </div>
+                <div>
+                    <div class="mb-1 font-semibold">Experience</div>
+                    <x-radio-group
+                        name="experience"
+                        :options="\App\Models\Job::$experience"
+                    />
+                </div>
+                <div>
+                    <div class="mb-1 font-semibold">Category</div>
+                    <x-radio-group
+                        name="category"
+                        :options="\App\Models\Job::$category"
+                    />
+                </div>
             </div>
 
-            <button type="submit" class="w-full">Filter</button>
+            <button
+                type="submit"
+                class="w-full"
+            >Filter</button>
         </form>
     </x-card>
 
