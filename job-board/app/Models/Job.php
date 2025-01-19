06bc11ff9f4c9e893595008ed61606afa0ad77bf
Alex <yuriev200@gmail.com>
2025-01-19 17:49:26 +0200

    Employer model & factory & seeder

--- job-board/app/Models/Job.php
+++ job-board/app/Models/Job.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Job extends Model
 {
@@ -12,4 +15,33 @@ class Job extends Model
 
     public static array $experience = ['entry', 'intermediate', 'senior'];
     public static array $category = ['IT', 'Finance', 'Sales', 'Marketing'];
+
+    public function employer(): BelongsTo {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function scopeFilter(
+        Builder | QueryBuilder $query,
+        array $filters
+    ): QueryBuilder | Builder {
+        return $query
+            ->when($filters['search'] ?? null, function ($query, $search) {
+                $query->where(function ($query) use ($search) {
+                    $query->where('title', 'like', '%' . $search . '%')
+                        ->orWhere('descr', 'like', '%' . $search . '%');
+                });
+            })
+            ->when($filters['min_salary'] ?? null, function ($query, $minSalary) {
+                $query->where('salary', '>=', $minSalary);
+            })
+            ->when($filters['max_salary'] ?? null, function ($query, $maxSalary) {
+                $query->where('salary', '<=', $maxSalary);
+            })
+            ->when($filters['experience'] ?? null, function ($query, $experience) {
+                $query->where('experience', $experience);
+            })
+            ->when($filters['category'] ?? null, function ($query, $category) {
+                $query->where('category', $category);
+            });
+    }
 }
