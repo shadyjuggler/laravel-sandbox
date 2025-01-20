1fe6b37409371225f62093bcb5f79b52f108112f
Alex <yuriev200@gmail.com>
2025-01-20 22:18:41 +0200

    logout function

--- job-board/app/Http/Controllers/AuthController.php
+++ job-board/app/Http/Controllers/AuthController.php
@@ -29,8 +29,13 @@ class AuthController extends Controller
         }
     }
 
-    public function destroy(string $id)
+    public function destroy()
     {
-        //
+        Auth::logout();
+
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
