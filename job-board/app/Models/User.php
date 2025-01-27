27f70d08749c4254c3fba5b189456242dda119f6
Alex <yuriev200@gmail.com>
2025-01-27 19:43:06 +0200

    job_applications migration & model & factory & seeder

--- job-board/app/Models/User.php
+++ job-board/app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -49,5 +50,9 @@ class User extends Authenticatable
 
     public function employer(): HasOne {
         return $this->hasOne(Employer::class);
+    }
+
+    public function jobApplications(): HasMany {
+        return $this->hasMany(JobApplication::class);
     }
 }
--- job-board/database/seeders/DatabaseSeeder.php
+++ job-board/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Employer;
 use App\Models\Job;
+use App\Models\JobApplication;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -37,5 +38,15 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
+        foreach($users as $user) {
+            $jobs = Job::inRandomOrder()->take(rand(0, 4))->get();
+
+            foreach($jobs as $job) {
+                JobApplication::factory()->create([
+                    'job_id' => $job->id,
+                    'user_id' => $user->id
+                ]);
+            }
+        }
     }
 }
